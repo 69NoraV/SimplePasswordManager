7b31d9471e249b99e28c47bef7343a20c94863a8
69NoraV <ragiofra2005@gmail.com>
2023-05-11 18:54:23 +0200

    0.0.3

---
+++ Source/Code/main.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int addpass();
+
+int main(void) {
+  bool loop=true;
+  // Main loop
+  while(loop) {
+    int selection;
+    // Main menu
+    printf("What you wanna do\n"
+           "1 - Add Password\n"
+           "2 - Search an exsisting password\n"
+           "3 - Delete Password\n"
+           "0 - Exit\n");
+    scanf("%d", &selection);
+    system("clear");
+    // Switch selection
+    switch(selection) {
+        case 1:
+            addpass(); // Call add password 
+            break;
+        case 0:
+            loop = false; // Exit the main loop
+            break;
+        default:
+            break;
+    }
+  }
+  
+}
+// Access to a file with the password and add a raw with the name and the password itself
+int addpass(void) {
+  system("clear");
+  
+}
+
